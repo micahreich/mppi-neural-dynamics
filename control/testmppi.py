ac6703b2f71a83a7d19fbf149090aca81fd4755a
micah reich <micahreich02@gmail.com>
2023-04-11 06:03:27 -0400

    tried numba

--- control/testmppi.py
+++ control/testmppi.py
@@ -2,109 +2,95 @@ import MPPIController
 import numpy as np
 import time
 import matplotlib.pyplot as plt
+import seaborn as sns
+from time import perf_counter
 
-plt.style.use('seaborn-darkgrid')
+
+sns.set_style("whitegrid")
+sns.set_palette("tab10")
 
 
-def block_on_slope(block_mass, slope_angle, gravity_accel, desired_state, dt):
+class BlockOnSlope:
     # Apply a force parallel to the slope to achieve a desired position
     # and velocity of the block
 
-    nx, nu = 2, 1
+    def __init__(self, initial_state, block_mass, slope_angle, g, dt, mu_k=0):
+        self.initial_state = initial_state
+        self.desired_state = None
+        self.block_mass = block_mass
+        self.slope_angle = slope_angle
+        self.g = g
+        self.dt = dt
+        self.mu_k = mu_k
+        self.nx, self.nu = (2, 1)
 
-    def terminal_cost(x):
-        error = (desired_state - x)
+        self.controller = MPPIController.MPPIController(
+            n_rollouts=200,
+            horizon_length=10,
+            exploration_cov=np.diag([10 ** 2]),
+            exploration_lambda=1e-2,
+            nx=self.nx,
+            nu=self.nu,
+            terminal_cost=self.terminal_cost,
+            state_cost=self.state_cost,
+            evolve_state=self.evolve_state,
+            dt=dt
+        )
+
+    def terminal_cost(self, x):
+        error = (self.desired_state - x)
         Q = np.array([[3, 0], [0, 1]])
+
         return error.T @ Q @ error
 
-    def state_cost(x):
-        error = (desired_state - x)
+    def state_cost(self, x):
+        error = (self.desired_state - x)
         Q = np.array([[1, 0], [0, 1]])
+
         return error.T @ Q @ error
 
-    def control_cost(u, noise):
-        noisy_control = u + noise
-        R = np.diag([0.5 * 1e-2])
-
-        return noisy_control.T @ R @ noisy_control
-
-    def evolve_state(x, u, dt):
+    def evolve_state(self, x, u, dt):
         pos, vel = x
+        accel = (self.g * np.sin(self.slope_angle)) + (u[0] / self.block_mass)
 
-        accel = (gravity_accel * np.sin(slope_angle)) + (u[0] / block_mass)
+        newvel = vel + accel * dt
+        newpos = pos + newvel * dt + 0.5 * accel * (dt ** 2)
 
-        evolved_state = np.array([
-            pos + vel * dt + 0.5 * accel * (dt ** 2),
-            vel + accel * dt
-        ])
+        evolved_state = np.array([newpos, newvel])
 
         return evolved_state
 
-    def simulator(x, u):
-        return evolve_state(x, u, dt=dt)
+    def simulator(self, x, u):
+        return self.evolve_state(x, u, dt=dt)
 
-    controller = MPPIController.MPPIController(
-        n_rollouts=200,
-        horizon_length=10,
-        exploration_cov=np.array([[10 ** 2]]),
-        exploration_lambda=5,
-        nx=nx,
-        nu=nu,
-        terminal_cost=terminal_cost,
-        state_cost=state_cost,
-        control_cost=control_cost,
-        control_cov=np.array([[1]]),
-        evolve_state=evolve_state,
-        dt=dt
-    )
 
-    return controller, simulator
+dt = 1 / 10
 
-# Create the block on slope controller
-
-sample_hz = 10
-dt = 1 / sample_hz
-desired_state = np.array([10, 0])
-
-controller, simulator = block_on_slope(
-    block_mass = 5, # kg
-    slope_angle = np.radians(30), # radians
-    gravity_accel = -9.81, # m / s^2
-    desired_state = desired_state,
-    dt = dt
+block_env = BlockOnSlope(
+    initial_state=np.array([0.0, 0.0]),
+    block_mass=5,               # kg
+    slope_angle=np.radians(30), # radians
+    g=-9.81,                     # m / s^2
+    dt=dt                       # s
 )
 
-# Create the control loop to run MPPI
+block_env.desired_state = np.array([10, 0])
 
-current_state = np.array([0, 0])  # [position, velocity]
-simulation_length = 15  # seconds
+# Create lists to store control sequence and state sequences for MPPI runs
+control_seq, states = [], []
 
-# Create some lists to hold plotted information
-control_seq = []
-xs = []
-
+simulation_length = 10  # s
 n_steps = int((1 / dt) * simulation_length)
 
-for i in range(0, n_steps):
-    next_u = controller.step(current_state)
-    control_seq.append(next_u[0])
+# Begin simulation
+current_state = block_env.initial_state
+start_time = perf_counter()
 
-    current_state = simulator(current_state, next_u)
-    xs.append(current_state[0])
+for _ in range(0, n_steps):
+    action = block_env.controller.step(current_state)
+    current_state = block_env.simulator(current_state, action)
 
-# Plot the results
+    states.append(current_state[0])
+    control_seq.append(action)
 
-fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(20, 10))
-fig.suptitle("Block on Slope Response")
-
-time = np.linspace(0, simulation_length, n_steps)
-
-ax1.plot(time, xs, label="block position")
-ax1.plot(time, np.repeat(desired_state[0], n_steps), label="desired position")
-ax1.set(xlabel="time (s)", ylabel="position (m)")
-ax1.legend()
-
-ax2.plot(time, control_seq, label="control")
-ax2.set(xlabel="time (s)", ylabel="force (N)")
-
-plt.show()
+print("Elapsed Time: {:.5f} s".format(perf_counter() - start_time))
